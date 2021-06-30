029da145f85da7206b578456a1512a647387da59
baraah2021 <>
2021-06-30 12:36:21 +0000

    added javascript

---
+++ app.js
@@ -0,0 +1,4 @@
+var favoriteAnime=prompt("what is your favorite anime?")
+alert(favoriteAnime)
+document.write("<h3>+favoriteAnime+</h3>")
+if(favoriteAnime!=null){document.write("<h3>+favoriteAnime+</h3>")} else alert("you did'nt write anything")
