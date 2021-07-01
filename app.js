a9f55c887156bf2e1616fb58c30783849a1823f6
baraah2021 <>
2021-07-01 12:30:38 +0000

    resubmitting lab04

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+alert("welcome to my website")
 var favoriteAnime=prompt("what is your favorite anime?")
-alert(favoriteAnime)
-document.write("<h3>+favoriteAnime+</h3>")
-if(favoriteAnime!=null){document.write("<h3>+favoriteAnime+</h3>")} else alert("you did'nt write anything")
+alert("cool!")
+
+if(favoriteAnime =="attack on titan") {
+  alert ("amazing!")
+}
+else if (favoriteAnime != null){
+  document.write("<h2>"+ favoriteAnime  + "</h2>")
+}
+else{
+  alert("you did not write anything")
+}
