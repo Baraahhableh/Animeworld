c2774264a9e702794153d81f35f5f2cd4a2ffc44
baraah2021 <>
2021-07-05 06:38:56 +0000

    functions

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-alert("welcome to my website")
-var favoriteAnime=prompt("what is your favorite anime?")
-alert("cool!")
+var guessnm= 10
+var ask= prompt ("Guess my favorite number")
+while (ask != guessnm){
+  ask= prompt("try again wrong answer!")
+}
+alert("you guessed it right")
 
-if(favoriteAnime =="attack on titan") {
-  alert ("amazing!")
-}
-else if (favoriteAnime != null){
-  document.write("<h2>"+ favoriteAnime  + "</h2>")
-}
-else{
-  alert("you did not write anything")
-}
+
+// var a = 5 
+// var b = 1
+
+// function sum () {
+// var c = a+b
+
+// return c;
+// }
+
+// var result = sum ()
+// console.log (result)
+
+
+var stars = prompt ("rate my website ");
+function star (y){
+  var answer = ' '
+  for ( var i = 0; i < y; i++) {
+    answer = answer + "<img src= 'https://lh3.googleusercontent.com/proxy/L8-0aSRZv0-jpaOWWMrRjif5SLi3LxGZAmtopkzgyTrm7Bhnrhj7x3w8wz7EHbdPICOFDZXHi-MXWhWFQzy1bchIyJDRHJp4dR6WWk3QxjUweg'>"
+  }
+return answer
+} 
+
+document.write(star(stars));
